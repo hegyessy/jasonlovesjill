1624d15ba0f5a938f2942ea3b9a1166b7928dd5f
Jason Hegyessy <hegyessy@apple.com>
2022-09-06 23:55:06 +0000

    Make text larger. New Icons.

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,11 +1,19 @@
 /** @jsx h */
 import { h } from "preact";
 import { tw } from "@twind";
+import { Head } from "$fresh/runtime.ts";
 
 export default function Home() {
   return (
-    <div class={tw`p-4 mx-auto bg-red-100 w-screen flex items-center justify-content h-screen`}>
-      <h1 class={tw`text-center w-full text-3xl`}>Jason ❤️ Jill</h1>
+
+    <div class={tw`bg-red-100 w-screen flex items-center justify-content h-screen`}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1" />
+        <link href="apple-touch-icon.png"
+        sizes="180x180"
+        rel="apple-touch-icon" />
+      </Head>
+      <h1 class={tw`text-center w-full text-[10vw]`}>Jason ❤️ Jill</h1>
     </div>
   );
 }
