3f98ce3b9819a23d0166ef13344d552e4fcf365b
Jason Hegyessy <jason@hegyessy.com>
2026-08-29 16:46:34 -0700

    prevent selectability on mobile

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,7 +3,11 @@ import Hearts from "../islands/Hearts.tsx";
 export default function Home() {
   return (
     <div class="bg-red-100 w-screen flex items-center justify-center overflow-hidden h-screen">
-      <img src="/thisisus.png" alt="Jason loves Jill" class="max-h-4/5" />
+      <img
+        src="/thisisus.png"
+        alt="Jason loves Jill"
+        class="max-h-4/5 select-none"
+      />
       <Hearts />
     </div>
   );
